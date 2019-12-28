cef06815a7d8abb8cf7b73017e101d46f557f646
xuziming <xuzimin_g@163.com>
2019-12-28 16:40:58 +0800

    init spring annotation

---
+++ src/main/java/com/atguigu/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.atguigu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.atguigu.service.BookService;
+
+@Controller
+public class BookController {
+
+	@Autowired
+	@SuppressWarnings("unused")
+	private BookService bookService;
+
+}
